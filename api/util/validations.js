ad3d785a40da72c5125299e3b4aa0b64ddfbfceb
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-10 18:21:06 -0500

    feat: :heavy_minus_sign: remove validator dependency

--- api/util/validations.js
+++ api/util/validations.js
@@ -1,11 +1,14 @@
-import validator from 'validator';
-
 export const isObjectEmpty = (obj) => {
     return Object.keys(obj).length === 0;
 }
 
 export const isNumber = (any) => {
     return !isNaN(Number(any));
+}
+
+export const isURL = (url) => {
+    var regex = /^(https?|https):\/\/[^\s/$.?#].[^\s]*$/i;
+    return regex.test(url);
 }
 
 export const isCampoValido = ({ campo, valor, tipoEsperado }) => {
@@ -15,7 +18,7 @@ export const isCampoValido = ({ campo, valor, tipoEsperado }) => {
         return { [campo]: new Date(valor).toISOString().split('T')[0] };
     };
     if (tipoEsperado == "url") {
-        if (validator.isURL(valor) || valor == "") return { [campo]: valor };
+        if (isURL(valor) || valor == "") return { [campo]: valor };
     }
     if (valor.constructor.name.toLowerCase() !== tipoEsperado.toLowerCase()) throw new Error(`Field: ${campo} - Value: ${valor} Does NOT correspond to the type of data requested`);
     return { [campo]: valor };
