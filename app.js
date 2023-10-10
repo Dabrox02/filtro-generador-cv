abe8d1508c9a65f5086d8fd6209d180c701f68e0
Jaider Mendoza <jaiderstyvenmendoza@gmail.com>
2023-10-10 07:38:32 -0500

    feat: :construction: form curriculum in progress

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const d = document;
 const $ = (e) => d.querySelector(e);
 
 export const app = async () => {
+    
+    new MultiSelectTag('countries')
 
     document.addEventListener("click", async (e) => {
         if (e.target.matches(".btn-close-modal")) {
